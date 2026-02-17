203c5473993bcb0c21bdfb9df7856a497b64dcaf
Chaitanya2872 <chaitanya.k@acstechnologies.co.in>
2026-02-17 23:09:45 +0530

    axios installed

--- config/secrets.js
+++ config/secrets.js
@@ -1,7 +1,7 @@
 // config/secrets.js
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || "supersecretkey1234567890",
-  homeApiBaseUrl: process.env.HOME_API_BASE_URL || "https://your-iotiq-api-base-url.com", // <-- SET THIS to your IOtiq Connect API base URL
+  homeApiBaseUrl: process.env.HOME_API_BASE_URL || "https://lht.iotiqinnovations.com", // <-- SET THIS to your IOtiq Connect API base URL
   accessTokenTTL: 3600,       // 1 hour
   refreshTokenTTL: 86400,     // 24 hours
   codeTTL: 600,               // 10 minutes
